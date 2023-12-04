c0099a4ac3158506bbd32b079e942da0845bd159
Eddie Wood <ewood@blltrust.com>
2023-12-04 10:31:10 -0700

    adding in Day 1 work

---
+++ Day1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<int> ints = new List<int>();
+
+var file = File.OpenText("PuzzleInput.txt");
+
+string? currentLine;
+currentLine = file.ReadLine();
+var count = 0;
+while (currentLine != null)
+{
+    var tensDigit = currentLine.First(c => Char.IsNumber(c));
+    var onesDigit = currentLine.Last(c => Char.IsNumber(c));
+    int theDigit = (tensDigit - '0')*10 + onesDigit - '0';
+    ints.Add(theDigit);
+    Console.WriteLine($"{currentLine}...{theDigit}");
+    currentLine = file.ReadLine();
+    count++;
+    //if (count > 4)
+    //    break;
+}
+
+Console.WriteLine(ints.Sum());
