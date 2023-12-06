a86f2addca78e7be0b13bc655bbe2f3301b12fd3
Eddie Wood <ewood@blltrust.com>
2023-12-05 18:55:11 -0700

    Day 5 part 2 works

--- Day5_2/hello.go
+++ Day5_2/hello.go
@@ -19,34 +19,89 @@ type Range struct {
 	End   int
 }
 
-func (std []SourceToDestination) GetDestinationRange(source Range) []Range []Range {
-	ranges := []Range{}
-	if source.Source >= std.Source && source.Source <= std.Source+std.Range-1 {
-		offset := source.Source - std.Source
-		return Range{Start: std.Destination + offset, End: std.Destination + offset + source.Range - 1}
-	}
-
-	return Range{Start: source.Source, End: source.Source}
-}
-
-func (std SourceToDestination) GetDestination(source *int) (int, bool) {
-	if *source >= std.Source && *source <= std.Source+std.Range-1 {
-		offset := *source - std.Source
-		return std.Destination + offset, true
-	}
-
-	return *source, false
-}
-
-func GetDestination(source *int, records *[]SourceToDestination) int {
-	for _, record := range *records {
-		destination, found := record.GetDestination(source)
-		if found {
-			return destination
+func (std SourceToDestination) GetRanges(tests []Range, elegantSolution bool) ([]Range, []Range) {
+	destinationRanges := []Range{}
+	noFoundSourceRanges := []Range{}
+	sourceStart := std.Source
+	sourceEnd := std.Source + std.Range - 1
+	if elegantSolution {
+		for _, test := range tests {
+			testStart := test.Start
+			testEnd := test.End
+			if testStart >= sourceStart && testEnd <= sourceEnd {
+				//fits neatly within range
+				offset := testStart - sourceStart
+				destinationRange := Range{std.Destination + offset, std.Destination + offset + testEnd - testStart}
+				destinationRanges = append(destinationRanges, destinationRange)
+				if destinationRange.Start > destinationRange.End {
+					fmt.Println("destination wrong: ", test, destinationRange, std)
+					panic("testStart >= sourceStart && testEnd <= sourceEnd")
+				}
+			} else if testEnd < sourceStart || testStart > sourceEnd {
+				//does not fit at all within range
+				noFoundSourceRanges = append(noFoundSourceRanges, test)
+				if test.Start > test.End {
+					fmt.Println("test wrong: ", test, std)
+					panic("testEnd < sourceStart || testStart > sourceEnd")
+				}
+			} else if testStart >= sourceStart && testEnd > sourceEnd {
+				//start fits in range, but end does not
+				noFoundSourceRange := Range{sourceEnd + 1, testEnd}
+				noFoundSourceRanges = append(noFoundSourceRanges, noFoundSourceRange)
+				offset := testStart - sourceStart
+				destinationRange := Range{std.Destination + offset, std.Destination + std.Range - 1}
+				destinationRanges = append(destinationRanges, destinationRange)
+				if destinationRange.Start > destinationRange.End {
+					fmt.Println("destination wrong: ", test, destinationRange, std)
+					panic("testStart >= sourceStart && testEnd > sourceEnd")
+				}
+				if noFoundSourceRange.Start > noFoundSourceRange.End {
+					fmt.Println("noFoundSourceRange wrong: ", test, noFoundSourceRange, std)
+					panic("testStart >= sourceStart && testEnd > sourceEnd")
+				}
+			} else if testStart < sourceStart && testEnd <= sourceEnd {
+				//end fits in range, but start does not
+				noFoundSourceRange := Range{testStart, sourceStart - 1}
+				noFoundSourceRanges = append(noFoundSourceRanges, noFoundSourceRange)
+				offset := testEnd - sourceStart
+				destinationRange := Range{std.Destination, std.Destination + offset}
+				destinationRanges = append(destinationRanges, destinationRange)
+				if destinationRange.Start > destinationRange.End {
+					fmt.Println("destination wrong: ", test, destinationRange, std)
+					panic("testStart < sourceStart && testEnd <= sourceEnd")
+				}
+				if noFoundSourceRange.Start > noFoundSourceRange.End {
+					fmt.Println("noFoundSourceRange wrong: ", test, noFoundSourceRange, std)
+					panic("testStart < sourceStart && testEnd <= sourceEnd")
+				}
+			} else if testStart < sourceStart && testEnd > sourceEnd {
+				//super wide....source extends past the start and end, but does overlap
+				noFoundSourceRangeAtStart := Range{testStart, sourceStart - 1}
+				noFoundSourceRanges = append(noFoundSourceRanges, noFoundSourceRangeAtStart)
+				noFoundSourceRangeAtEnd := Range{sourceEnd + 1, testEnd}
+				noFoundSourceRanges = append(noFoundSourceRanges, noFoundSourceRangeAtEnd)
+				destinationRange := Range{std.Destination, std.Destination + std.Range - 1}
+				destinationRanges = append(destinationRanges, destinationRange)
+				if destinationRange.Start > destinationRange.End {
+					fmt.Println("destination wrong: ", test, destinationRange, std)
+					panic("testStart < sourceStart && testEnd <= sourceEnd")
+				}
+				if noFoundSourceRangeAtStart.Start > noFoundSourceRangeAtStart.End {
+					fmt.Println("noFoundSourceRangeAtStart wrong: ", test, noFoundSourceRangeAtStart, std)
+					panic("testStart < sourceStart && testEnd <= sourceEnd")
+				}
+				if noFoundSourceRangeAtEnd.Start > noFoundSourceRangeAtEnd.End {
+					fmt.Println("noFoundSourceRangeAtEnd wrong: ", test, noFoundSourceRangeAtEnd, std)
+					panic("testStart < sourceStart && testEnd <= sourceEnd")
+				}
+			} else {
+				fmt.Println("ALL WRONG: ", test, std)
+				fmt.Println(sourceStart, sourceEnd)
+				panic("why are we here?")
+			}
 		}
 	}
-
-	return *source
+	return destinationRanges, noFoundSourceRanges
 }
 
 func main() {
@@ -78,7 +133,6 @@ func main() {
 			seeds = append(seeds, seed)
 		}
 	}
-	fmt.Println(seeds)
 	scanner.Scan()
 
 	analysis := make(map[string][]SourceToDestination)
@@ -87,15 +141,12 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		if len(line) == 0 {
-			//fmt.Println("Key Len was Zero")
 			analysis[key] = records
 			records = []SourceToDestination{}
 			key = ""
 		} else if strings.Contains(line, ":") {
-			//fmt.Println("Key Len was not Zero")
 			key = line
 		} else {
-			//fmt.Println("Tokenizing")
 			tokens := strings.Split(line, " ")
 			destination, _ := strconv.Atoi(tokens[0])
 			source, _ := strconv.Atoi(tokens[1])
@@ -104,15 +155,11 @@ func main() {
 			records = append(records, std)
 		}
 	}
-	//fmt.Println(len(key))
 	if len(key) > 0 {
 		analysis[key] = records
 	}
 
-	lowestSeedvalue := 999999999
-
 	seedStart := 0
-	seedRange := 0
 	seedtosoilmap := analysis[seed_to_soil]
 	soiltofertilizermap := analysis[soil_to_fertilizer]
 	ferttowatermap := analysis[fertilizer_to_water]
@@ -121,70 +168,39 @@ func main() {
 	temptohumid := analysis[temperature_to_humidity]
 	humidtoloc := analysis[humidity_to_location]
 
-	// for _, record := range humidtoloc {
-
-	// }
+	noDestinationDetermined := []Range{}
 	for index, value := range seeds {
 		if index%2 == 0 {
 			seedStart = value
 		} else {
-			seedRange = value
+			seedRange := Range{seedStart, seedStart + value - 1}
+			noDestinationDetermined = append(noDestinationDetermined, seedRange)
+		}
+	}
 
-			for seed := seedStart; seed < seedStart+seedRange; seed++ {
-				fmt.Println("seed ", seed)
-				destination := GetDestination(&seed, &seedtosoilmap)
-				//fmt.Print("->", destination)
-				destination = GetDestination(&destination, &soiltofertilizermap)
-				//fmt.Print("->", destination)
-				destination = GetDestination(&destination, &ferttowatermap)
-				//fmt.Print("->", destination)
-				destination = GetDestination(&destination, &watertolight)
-				//fmt.Print("->", destination)
-				destination = GetDestination(&destination, &lighttotemp)
-				//fmt.Print("->", destination)
-				destination = GetDestination(&destination, &temptohumid)
-				//fmt.Print("->", destination)
-				destination = GetDestination(&destination, &humidtoloc)
-				//fmt.Println("->", destination)
-				if destination < lowestSeedvalue {
-					lowestSeedvalue = destination
-				}
+	destinationMaps := [][]SourceToDestination{seedtosoilmap, soiltofertilizermap, ferttowatermap, watertolight, lighttotemp, temptohumid, humidtoloc}
+
+	destinations := []Range{}
+	for _, destinationMap := range destinationMaps {
+		for _, std := range destinationMap {
+			tempDest, tempNoDestinationDetermined := std.GetRanges(noDestinationDetermined, true)
+			noDestinationDetermined = tempNoDestinationDetermined
+			for _, tempdest := range tempDest {
+				destinations = append(destinations, tempdest)
 			}
+		}
+		//get ready for next round of destination checking
+		for _, dest := range destinations {
+			noDestinationDetermined = append(noDestinationDetermined, dest)
+		}
+		destinations = nil
+	}
+
+	lowestSeedvalue := 999999999
+	for _, source := range noDestinationDetermined {
+		if source.Start < lowestSeedvalue {
+			lowestSeedvalue = source.Start
 		}
 	}
 	fmt.Println(lowestSeedvalue)
-	//346433842
-	// for row := 0; row < len(records); row++ {
-	// 	for col := 0; col < len(records[row]); col++ {
-	// 		if unicode.IsDigit(records[row][col]) {
-	// 			tokenEnd := col
-	// 			for ; tokenEnd < len(records[row]) && unicode.IsDigit(records[row][tokenEnd]); tokenEnd++ {
-	// 			}
-	// 			digits := string(records[row][col:tokenEnd])
-	// 			number, _ := strconv.Atoi(digits)
-	// 			//fmt.Println(number)
-	// 			for scanRow := max(0, row-1); scanRow <= min(row+1, len(records)-1); scanRow++ {
-	// 				for scanCol := max(0, col-1); scanCol <= min(col+len(digits), len(records[row])-1); scanCol++ {
-	// 					if records[scanRow][scanCol] != '.' && !unicode.IsDigit(records[scanRow][scanCol]) {
-	// 						potentialGears = append(potentialGears, Gear{number, scanRow, scanCol})
-	// 						goto foundSymbol
-	// 					}
-	// 				}
-	// 			}
-	// 		foundSymbol:
-	// 			col = tokenEnd
-	// 		}
-	// 	}
-	// }
-
-	// totalPower := 0
-	// for index, gear := range potentialGears {
-	// 	for _, otherGear := range potentialGears[index+1:] {
-	// 		if gear.Col == otherGear.Col && gear.Row == otherGear.Row {
-	// 			totalPower += gear.Number * otherGear.Number
-	// 		}
-	// 	}
-	// }
-
-	// fmt.Println(totalPower)
 }
