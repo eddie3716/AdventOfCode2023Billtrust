6bc23fb64bb082afd12f0861830b9b57d9529849
Eddie Wood <ewood@blltrust.com>
2023-12-13 23:30:02 -0700

    Day12_1 was working, then i broke it trying to get part 2 to work.

--- Day12/hello.go
+++ Day12/hello.go
@@ -9,67 +9,122 @@ import (
 )
 
 func main() {
-	data := parseFile("testinput.txt")
+	sets := parseFile("testinput.txt")
 
-	part1(data)
+	part1(sets)
 
-	//part2(gridPatterns)
-}
-
-func WillFit(t string, number rune, index int) (bool, int) {
-	skew := int(number) - 1
-
-	if skew < 0 {
-		fmt.Println(skew, " ", number, " ", index, " ", t)
-		panic("will not fit")
-	}
-
-	endIndex := index + skew
-
-	if endIndex >= len(t) {
-		fmt.Println(skew, " ", number, " ", index, " ", t)
-		panic("How does this even happen")
-	}
-
-	nextIndex := index + skew + 1
-	testFirstSymbol := t[index]
-	testEndSymbol := t[endIndex]
-	switch testFirstSymbol {
-	case '?':
-		if testEndSymbol == '?' || (testEndSymbol == '#' && nextIndex < len(t) && t[nextIndex] != '#') {
-			return true, nextIndex
-		} else {
-			return false, index + 1
-		}
-	case '#':
-		if (testEndSymbol == '?') || (testEndSymbol == '#' && nextIndex < len(t) && t[nextIndex] != '#') {
-			return true, nextIndex
-		} else {
-			return false, index + 1
-		}
-	default:
-		fmt.Println(skew, " ", number, " ", index, " ", testFirstSymbol, " ", testEndSymbol, t)
-		panic("Unknown symbol")
-	}
-	fmt.Println(skew, " ", number, " ", index, " ", testFirstSymbol, " ", testEndSymbol, t)
-	panic("Should not be here")
+	part2(sets)
 }
 
 type Pattern []int
+type Template string
 
-type Set struct {
-	Pattern  []int
-	Template string
+func (t Template) replaceAtIndex(r rune, index int, number int) (string, int) {
+	out := []rune(t)
+	new := 0
+	for i := index; i < index+number; i++ {
+		if out[i] != r {
+			out[i] = r
+			new++
+		}
+	}
+	return string(out), new
 }
 
-func (s Set) FindCombinations(start int) (int, bool) {
-	found := true
+type Set struct {
+	Pattern        []int
+	Template       Template
+	StartingPounds int
+	ExpectedPounds int
+}
 
-	if found {
+func NewSet(pattern Pattern, template Template) Set {
+	set := Set{pattern, template, 0, 0}
 
-		newSet := Set{s.Pattern[start+1:], s.Template[start+1:]}
-		return newSet.FindCombinations(start + 1)
+	for _, r := range template {
+		if r == '#' {
+			set.StartingPounds++
+		}
 	}
+
+	for _, r := range pattern {
+		set.ExpectedPounds += r
+	}
+
+	return set
+}
+
+func (s Set) Fold() Set {
+	newTemplate := s.Template + "?" + s.Template + "?" + s.Template + "?" + s.Template + "?" + s.Template
+	newPattern := []int{}
+	newPattern = append(newPattern, s.Pattern...)
+	newPattern = append(newPattern, s.Pattern...)
+	newPattern = append(newPattern, s.Pattern...)
+	newPattern = append(newPattern, s.Pattern...)
+	newPattern = append(newPattern, s.Pattern...)
+	newSet := NewSet(newPattern, Template(newTemplate))
+	return newSet
+}
+
+func (s Set) PatternFits(iPattern, iTemplate int) bool {
+
+	if iPattern >= len(s.Pattern) {
+		return false
+	}
+
+	num := s.Pattern[iPattern]
+	endIndex := iTemplate + num - 1
+	if endIndex >= len(s.Template) {
+		return false
+	}
+
+	if (s.Template[iTemplate] == '#' || s.Template[iTemplate] == '?') && (iTemplate == 0 || s.Template[iTemplate-1] != '#') && (endIndex == len(s.Template)-1 || s.Template[endIndex+1] != '#') {
+
+		for i := iTemplate; i <= endIndex; i++ {
+			if s.Template[i] == '.' {
+				return false
+			}
+		}
+
+		return true
+	}
+	return false
+}
+
+func (s Set) FindCombos() int {
+	cache := make(map[int]bool)
+	return s.FindCombinations(0, 0, 0, cache)
+}
+
+func (s Set) FindCombinations(iPattern int, iTemplate int, depth int, cache map[int]bool) int {
+	if depth == 0 {
+		fmt.Println("Set ", s, " starting...")
+	}
+
+	if iTemplate > len(s.Template) {
+		return 0
+	}
+	combos := 0
+	newDepth := depth + 1
+	itFits := s.PatternFits(iPattern, iTemplate)
+	if itFits {
+		newTemplate, newPounds := s.Template.replaceAtIndex('#', iTemplate, s.Pattern[iPattern])
+		if s.StartingPounds+newPounds == s.ExpectedPounds {
+			//s.Template = Template(strings.ReplaceAll(string(s.Template), "?", "."))
+			fmt.Println("Set ", s, " match")
+			combos++
+		}
+		newSet := Set{s.Pattern, Template(newTemplate), s.StartingPounds + newPounds, s.ExpectedPounds}
+
+		combos += newSet.FindCombinations(iPattern+1, iTemplate+s.Pattern[iPattern], newDepth, cache)
+	}
+
+	combos += s.FindCombinations(iPattern, iTemplate+1, newDepth, cache)
+
+	if depth == 0 {
+		fmt.Println("Set ", s, " has ", combos, " combinations")
+	}
+	return combos
 }
 
 type Sets []Set
@@ -101,46 +156,29 @@ func parseFile(fileName string) Sets {
 			number, _ := strconv.Atoi(token)
 			pattern = append(pattern, number)
 		}
-		set := Set{pattern, Template(tokens[0])}
+		set := NewSet(pattern, Template(tokens[0]))
 		sets = append(sets, set)
 	}
 
-	sets.PrintGridPatterns()
+	//sets.PrintGridPatterns()
 	return sets
 }
 
 func part1(sets Sets) {
 
-	for index, pattern := range sets.Patterns {
-		templates := sets.Grid[index]
-		iTemplates := 0
-		iTemplate := 0
-		iNumber := 0
-		willFit := true
-		currentTemplate := templates[iTemplates]
-		for iNumber = 0; iNumber < len(pattern); iNumber++ {
-			number := pattern[iNumber]
-			if iTemplate >= len(currentTemplate) {
-				iTemplates++
-				currentTemplate = templates[iTemplates]
-				iTemplate = 0
-			}
-			fmt.Println(number, " ", currentTemplate, " ", iTemplate)
-			tempITemplate := iTemplate
-			willFit, iTemplate = currentTemplate.WillFit(rune(number), iTemplate)
-			if willFit {
-				[]rune(currentTemplate)[tempITemplate] = '#'
-			}
-			if !willFit {
-				fmt.Println("No match")
-				break
-			}
-		}
-
-		fmt.Println(gridPatterns)
+	totalCombinations := 0
+	for _, set := range sets {
+		totalCombinations += set.FindCombos()
 	}
+
+	fmt.Println("Part 1: ", totalCombinations)
 }
 
-func part2(gridPatterns Sets) {
-	fmt.Println("Part 2")
+func part2(sets Sets) {
+	totalCombinations := 0
+	// for _, set := range sets {
+	// 	totalCombinations += set.Fold().FindCombos()
+	// }
+
+	fmt.Println("Part 1: ", totalCombinations)
 }
