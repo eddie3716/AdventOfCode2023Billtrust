9822bcb901b0beed205653d7becb0f43ea2a45db
Eddie Wood <ewood@blltrust.com>
2023-12-19 11:07:19 -0700

    Day17 optimized and working

--- Day17/hello.go
+++ Day17/hello.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"strconv"
-	"strings"
 )
 
+type validDirection func(int) bool
+
+var endRow int
+var endCol int
 var heatMap [][]int = [][]int{}
 var directions = map[byte][]int{NORTH: toNorth, SOUTH: toSouth, WEST: toWest, EAST: toEast}
 
@@ -25,70 +27,73 @@ var toSouth = []int{1, 0}
 var toWest = []int{0, -1}
 var toEast = []int{0, 1}
 
-type CityBlock struct {
+type CityBlockState struct {
 	iRow                    int
 	iCol                    int
-	totalHeat               int
 	stepsInCurrentDirection int
 	direction               byte
+}
+
+type CityBlock struct {
+	totalHeat int
+	state     CityBlockState
 }
 
 func (cb *CityBlock) NextCityBlock(mnemonicDirection byte) *CityBlock {
 	var direction []int = directions[mnemonicDirection]
 
-	stepsInCurrentDirection := 0
-	if mnemonicDirection == cb.direction {
-		stepsInCurrentDirection = cb.stepsInCurrentDirection + 1
-	} else {
-		stepsInCurrentDirection = 1
+	stepsInCurrentDirection := 1
+	if mnemonicDirection == cb.state.direction {
+		stepsInCurrentDirection += cb.state.stepsInCurrentDirection
 	}
-	return &CityBlock{iRow: cb.iRow + direction[0], iCol: cb.iCol + direction[1], totalHeat: cb.totalHeat + heatMap[cb.iRow+direction[0]][cb.iCol+direction[1]], direction: mnemonicDirection, stepsInCurrentDirection: stepsInCurrentDirection}
+
+	return &CityBlock{state: CityBlockState{iRow: cb.state.iRow + direction[0], iCol: cb.state.iCol + direction[1], direction: mnemonicDirection, stepsInCurrentDirection: stepsInCurrentDirection}, totalHeat: cb.totalHeat + heatMap[cb.state.iRow+direction[0]][cb.state.iCol+direction[1]]}
 }
 
-func (cb *CityBlock) GetNextDirections(heatMap [][]int) []*CityBlock {
+func (cb *CityBlock) GetNextDirections(heatMap [][]int, min int, max int) *[]*CityBlock {
 	directions := []*CityBlock{}
-	if cb.direction == NORTH {
-		if cb.stepsInCurrentDirection < 3 && cb.iRow > 0 {
+	if cb.state.direction == NORTH {
+		if cb.state.stepsInCurrentDirection < max && cb.state.iRow > 0 {
 			directions = append(directions, cb.NextCityBlock(NORTH))
 		}
-		if cb.iCol > 0 {
+		if cb.state.stepsInCurrentDirection >= min && cb.state.iCol > 0 {
 			directions = append(directions, cb.NextCityBlock(WEST))
 		}
-		if cb.iCol < len(heatMap[0])-1 {
+		if cb.state.stepsInCurrentDirection >= min && cb.state.iCol < len(heatMap[0])-1 {
 			directions = append(directions, cb.NextCityBlock(EAST))
 		}
-	} else if cb.direction == SOUTH {
-		if cb.stepsInCurrentDirection < 3 && cb.iRow < len(heatMap)-1 {
+	} else if cb.state.direction == SOUTH {
+		if cb.state.stepsInCurrentDirection < max && cb.state.iRow < endRow {
 			directions = append(directions, cb.NextCityBlock(SOUTH))
 		}
-		if cb.iCol > 0 {
+		if cb.state.stepsInCurrentDirection >= min && cb.state.iCol > 0 {
 			directions = append(directions, cb.NextCityBlock(WEST))
 		}
-		if cb.iCol < len(heatMap[0])-1 {
+		if cb.state.stepsInCurrentDirection >= min && cb.state.iCol < len(heatMap[0])-1 {
 			directions = append(directions, cb.NextCityBlock(EAST))
 		}
-	} else if cb.direction == WEST {
-		if cb.stepsInCurrentDirection < 3 && cb.iCol > 0 {
+	} else if cb.state.direction == WEST {
+		if cb.state.stepsInCurrentDirection < max && cb.state.iCol > 0 {
 			directions = append(directions, cb.NextCityBlock(WEST))
 		}
-		if cb.iRow > 0 {
+		if cb.state.stepsInCurrentDirection >= min && cb.state.iRow > 0 {
 			directions = append(directions, cb.NextCityBlock(NORTH))
 		}
-		if cb.iRow < len(heatMap)-1 {
+		if cb.state.stepsInCurrentDirection >= min && cb.state.iRow < len(heatMap)-1 {
 			directions = append(directions, cb.NextCityBlock(SOUTH))
 		}
-	} else if cb.direction == EAST {
-		if cb.stepsInCurrentDirection < 3 && cb.iCol < len(heatMap[0])-1 {
+	} else if cb.state.direction == EAST {
+		if cb.state.stepsInCurrentDirection < max && cb.state.iCol < endCol {
 			directions = append(directions, cb.NextCityBlock(EAST))
 		}
-		if cb.iRow > 0 {
+		if cb.state.stepsInCurrentDirection >= min && cb.state.iRow > 0 {
 			directions = append(directions, cb.NextCityBlock(NORTH))
 		}
-		if cb.iRow < len(heatMap)-1 {
+		if cb.state.stepsInCurrentDirection >= min && cb.state.iRow < len(heatMap)-1 {
 			directions = append(directions, cb.NextCityBlock(SOUTH))
 		}
 	}
-	return directions
+	return &directions
 }
 
 type PriorityQueue []*CityBlock
@@ -113,11 +118,11 @@ func (pq *PriorityQueue) Pop() interface{} {
 
 func main() {
 	parseFile("input.txt")
-	printHeatMap(heatMap)
+	//printHeatMap(heatMap)
 
-	part1()
+	fmt.Println("answer part 1:", findMinHeat(0, 3))
 
-	// part2()
+	fmt.Println("answer part 2:", findMinHeat(4, 10))
 }
 
 func parseFile(fileName string) {
@@ -139,6 +144,9 @@ func parseFile(fileName string) {
 		}
 		heatMap = append(heatMap, record)
 	}
+
+	endRow = len(heatMap) - 1
+	endCol = len(heatMap[0]) - 1
 }
 
 func printHeatMap(heatMap [][]int) {
@@ -151,14 +159,6 @@ func printHeatMap(heatMap [][]int) {
 	fmt.Println()
 }
 
-func (cb *CityBlock) String() string {
-	return fmt.Sprintf("(col:%d,row:%d,dir:%q,theat:%d)", cb.iRow, cb.iCol, cb.direction, cb.totalHeat)
-}
-
-func (cb *CityBlock) GetKey() string {
-	return fmt.Sprintf("%d,%d,%d,%d", cb.iRow, cb.iCol, cb.direction, cb.stepsInCurrentDirection)
-}
-
 func Min(x, y int) int {
 	if x < y {
 		return x
@@ -166,47 +166,39 @@ func Min(x, y int) int {
 	return y
 }
 
-func part1() {
+func findMinHeat(min int, max int) int {
 
 	answer := math.MaxInt
-	visited := map[string]int{}
+	visited := map[CityBlockState]int{}
 	pq := make(PriorityQueue, 0)
 	heap.Init(&pq)
-	heap.Push(&pq, &CityBlock{iRow: 0, iCol: 0, direction: EAST, stepsInCurrentDirection: 0, totalHeat: 0})
+	heap.Push(&pq, &CityBlock{state: CityBlockState{iRow: 0, iCol: 0, direction: EAST, stepsInCurrentDirection: 0}, totalHeat: 0})
 
 	for len(pq) > 0 {
 		currentCityBlock := heap.Pop(&pq).(*CityBlock)
-		newDirections := currentCityBlock.GetNextDirections(heatMap)
 
-		key := currentCityBlock.GetKey()
-		if visitedHeat, didVisit := visited[key]; didVisit && visitedHeat < currentCityBlock.totalHeat {
+		if _, didVisit := visited[currentCityBlock.state]; didVisit {
 			continue
 		}
 
-		visited[key] = currentCityBlock.totalHeat
+		visited[currentCityBlock.state] = currentCityBlock.totalHeat
 
-		for _, newDirection := range newDirections {
+		newDirections := currentCityBlock.GetNextDirections(heatMap, min, max)
+
+		for _, newDirection := range *newDirections {
+			//fmt.Println(newDirection.state, newDirection.totalHeat)
 			heap.Push(&pq, newDirection)
 		}
 	}
 	//fmt.Println("Visited: ", visited)
-	for key, value := range visited {
-		tokens := strings.Split(key, ",")
-		row, _ := strconv.Atoi(tokens[0])
-		col, _ := strconv.Atoi(tokens[1])
-		if row == len(heatMap[0])-1 && col == len(heatMap)-1 {
+	for state, value := range visited {
+		//fmt.Println(state, value)
+		if state.iRow == len(heatMap)-1 && state.iCol == len(heatMap[0])-1 {
 			answer = Min(answer, value)
 		}
 	}
 
-	fmt.Println()
-	printHeatMap(heatMap)
-	fmt.Println("Part 1: ", answer)
-}
-
-func part2(heatMap [][]int) {
-
-	answer := 0
-
-	fmt.Println("Part 2: ", answer)
+	//fmt.Println()
+	//printHeatMap(heatMap)
+	return answer
 }
