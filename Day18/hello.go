2dd1a6943e93682e5d239135dbb58b13b4f9d67b
Eddie Wood <ewood@blltrust.com>
2023-12-20 20:56:49 -0700

    Day18 done

--- Day18/hello.go
+++ Day18/hello.go
@@ -8,97 +8,69 @@ import (
 	"strings"
 )
 
-type Instructions struct {
+type Instruction struct {
 	direction string
-	steps     int
+	steps     int64
 }
 
 type Digplan struct {
-	instructions []Instructions
-	colors       []string
-	maxRows      int
-	maxCols      int
-	startingRow  int
-	grid         [][]rune
+	instructions     []Instruction
+	realInstructions []Instruction
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
+type point struct{ x, y float64 }
+
+func shoelace(points []point) float64 {
+	n := len(points)
+	if n < 3 {
+		return 0 // not a polygon
 	}
-	return b
+
+	sum := 0.0
+	for i := 0; i < n; i++ {
+		j := (i + 1) % n
+		sum += (points[i].x * points[j].y) - (points[j].x * points[i].y)
+	}
+	return 0.5 * sum
 }
 
-func (digPlan *Digplan) generateGrid() {
-	maxRows := 0
-	maxCols := 0
-	deltaRows := 0
-	deltaCols := 0
+func computeArea(instructions []Instruction) float64 {
+	var x int64 = 0
+	var y int64 = 0
+	perimeter := 0.
 
-	deltaFromStart := 0
-	maxDeltaFromStart := 0
-	for _, instruction := range digPlan.instructions {
+	points := []point{}
+	points = append(points, point{float64(x), float64(y)})
+	for _, instruction := range instructions {
+		perimeter += float64(instruction.steps)
+		points = append(points, point{float64(x), float64(y)})
 		switch instruction.direction {
 		case "R":
-			deltaCols += instruction.steps
-			maxCols = max(maxCols, deltaCols)
+			x += instruction.steps
 		case "L":
-			deltaCols -= instruction.steps
+			x -= instruction.steps
 		case "U":
-			deltaFromStart += instruction.steps
-			maxDeltaFromStart = max(maxDeltaFromStart, deltaFromStart)
-			deltaRows -= instruction.steps
+			y -= instruction.steps
 		case "D":
-			deltaFromStart -= instruction.steps
-			maxDeltaFromStart = max(maxDeltaFromStart, deltaFromStart)
-			deltaRows += instruction.steps
-			maxRows = max(maxRows, deltaRows)
+			y += instruction.steps
 		}
 	}
 
-	if maxRows <= 0 {
-		panic("negative rows")
-	}
+	//Pick's theorem having a baby with shoelace
+	area := shoelace(points) + perimeter/2 + 1
 
-	if maxCols <= 0 {
-		panic("negative cols")
-	}
-
-	digPlan.maxRows = maxRows + 1
-	digPlan.maxCols = maxCols + 1
-	digPlan.startingRow = maxDeltaFromStart
-
-	digPlan.grid = make([][]rune, digPlan.maxRows)
-	for i := range digPlan.grid {
-		digPlan.grid[i] = make([]rune, digPlan.maxCols)
-		for j := range digPlan.grid[i] {
-			digPlan.grid[i][j] = '.'
-		}
-	}
+	return area
 }
-
-var digPlan Digplan = Digplan{colors: []string{}, instructions: []Instructions{}}
 
 func main() {
-	parseFile("input.txt")
+	digPlan := parseFile("input.txt")
 
-	fmt.Println("answer part 1:", findMinHeat())
+	fmt.Println("answer part 1:", int64(computeArea(digPlan.instructions)))
 
-	//fmt.Println("answer part 2:", findMinHeat())
+	fmt.Println("answer part 2:", int64(computeArea(digPlan.realInstructions)))
 }
 
-func (digPlan *Digplan) String() string {
-	var sb strings.Builder
-	for _, row := range digPlan.grid {
-		for _, col := range row {
-			sb.WriteRune(col)
-		}
-		sb.WriteString("\n")
-	}
-	return sb.String()
-}
-
-func parseFile(fileName string) {
+func parseFile(fileName string) Digplan {
 	file, err := os.Open(fileName)
 
 	if err != nil {
@@ -109,76 +81,42 @@ func parseFile(fileName string) {
 
 	scanner := bufio.NewScanner(file)
 
+	digPlan := Digplan{realInstructions: []Instruction{}, instructions: []Instruction{}}
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		tokens := strings.Split(line, " ")
 		direction := tokens[0]
 		steps, _ := strconv.Atoi(tokens[1])
-		instruction := Instructions{direction: direction, steps: steps}
+		instruction := Instruction{direction: direction, steps: int64(steps)}
 		digPlan.instructions = append(digPlan.instructions, instruction)
-		color := tokens[2][1:]
-		digPlan.colors = append(digPlan.colors, color)
-	}
-	digPlan.generateGrid()
-}
-
-func findMinHeat() int {
-	fmt.Println(digPlan.String())
-	answer := 0
-
-	currentRow := digPlan.startingRow
-	currentCol := 0
-	digPlan.grid[currentRow][currentCol] = '#'
-	for _, instruction := range digPlan.instructions {
-		switch instruction.direction {
-		case "R":
-			for i := 0; i < instruction.steps; i++ {
-				currentCol++
-				digPlan.grid[currentRow][currentCol] = '#'
-				answer++
-			}
-		case "L":
-			for i := 0; i < instruction.steps; i++ {
-				currentCol--
-				digPlan.grid[currentRow][currentCol] = '#'
-				answer++
-			}
-		case "U":
-			for i := 0; i < instruction.steps; i++ {
-				currentRow--
-				digPlan.grid[currentRow][currentCol] = '#'
-				answer++
-			}
-		case "D":
-			for i := 0; i < instruction.steps; i++ {
-				currentRow++
-				digPlan.grid[currentRow][currentCol] = '#'
-				answer++
-			}
+		hexSteps := tokens[2][2 : len(tokens[2])-2]
+		realSteps, err := strconv.ParseInt(hexSteps, 16, 32)
+		dirCode := tokens[2][len(tokens[2])-2 : len(tokens[2])-1]
+		realDir := ""
+		switch dirCode {
+		case "0":
+			realDir = "R"
+			break
+		case "1":
+			realDir = "D"
+			break
+		case "2":
+			realDir = "L"
+			break
+		case "3":
+			realDir = "U"
+			break
 		}
-	}
-	fmt.Println(digPlan.String())
-	for i := 0; i < digPlan.maxRows; i++ {
-		previousChar := '?'
-		inside := false
-		for j := 0; j < digPlan.maxCols; j++ {
+		realInstruction := Instruction{direction: realDir, steps: realSteps}
 
-			currentChar := digPlan.grid[i][j]
-
-			if previousChar == '#' && currentChar == '.' {
-				inside = !inside
-			}
-
-			previousChar = currentChar
-			if inside && currentChar == '.' {
-				digPlan.grid[i][j] = '#'
-				answer++
-			}
+		if err != nil {
+			panic(err)
 		}
+		digPlan.realInstructions = append(digPlan.realInstructions, realInstruction)
 	}
 
-	fmt.Println(digPlan.String())
+	fmt.Println(digPlan)
 
-	//fmt.Println(digPlan)
-	return answer
+	return digPlan
 }
