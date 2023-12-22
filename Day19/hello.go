5d828eecc9f0adc1a8981db4fd589374b296d4a4
Eddie Wood <ewood@blltrust.com>
2023-12-21 17:17:00 -0700

    Day19 wip

--- Day19/hello.go
+++ Day19/hello.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
+
+	orderedmap "github.com/wk8/go-ordered-map/v2"
 )
 
 type Part struct {
@@ -15,14 +16,82 @@ type Part struct {
 	s int
 }
 
-type Workflow struct {
+func greaterThan(a, b int) bool {
+	return a > b
 }
 
-func part1(parts []Part, workflows map[string]Workflow) int {
+func lessThan(a, b int) bool {
+	return a < b
+}
+
+func nop(a, b int) bool {
+	return true
+}
+
+const (
+	GreaterThan = "GreaterThan"
+	LessThan    = "LessThan"
+	Result      = "Result"
+)
+
+type Node struct {
+	function    func(a, b int) bool
+	operandType string
+	operand2    int
+	next        string
+}
+
+func getFunction(nodeType string) func(a, b int) bool {
+	switch nodeType {
+	case GreaterThan:
+		return greaterThan
+	case LessThan:
+		return lessThan
+	case Result:
+		return nop
+	default:
+		panic("unknown node type: " + nodeType)
+	}
+}
+
+func (workflow *Workflow) evaluate(part *Part, workflows *orderedmap.OrderedMap[string, *Workflow]) string {
+	for _, node := range *workflow.node {
+		operand1 := -1
+		switch node.operandType {
+		case "a":
+			operand1 = part.a
+			break
+		case "x":
+			operand1 = part.x
+			break
+		case "m":
+			operand1 = part.m
+			break
+		case "s":
+			operand1 = part.s
+		}
+
+		if node.function(operand1, node.operand2) {
+			if nextNode, present := workflows.Get(node.next); present {
+				return nextNode.evaluate(part, workflows)
+			} else {
+				return node.next
+			}
+		}
+	}
+
+	return "0"
+}
+
+type Workflow struct {
+	node *[]Node
+}
+
+func part1(parts *[]Part, workflows *orderedmap.OrderedMap[string, *Workflow]) int {
 	return 0
 }
 
-func part2(parts []Part, workflows map[string]Workflow) int {
+func part2(parts *[]Part, workflows *orderedmap.OrderedMap[string, *Workflow]) int {
 	return 0
 }
 
@@ -34,7 +103,12 @@ func main() {
 	fmt.Println("answer part 2:", part2(parts, workflows))
 }
 
-func parseFile(fileName string) ([]Part, map[string]Workflow) {
+func parseWorkFlow(workflowString string) *Workflow {
+	workflow := Workflow{}
+	return &workflow
+}
+
+func parseFile(fileName string) (*[]Part, *orderedmap.OrderedMap[string, *Workflow]) {
 	file, err := os.Open(fileName)
 
 	if err != nil {
@@ -45,42 +119,26 @@ func parseFile(fileName string) ([]Part, map[string]Workflow) {
 
 	scanner := bufio.NewScanner(file)
 
-	digPlan := Digplan{realInstructions: []Instruction{}, instructions: []Instruction{}}
+	workFlows := orderedmap.New[string, *Workflow]()
+	parts := []Part{}
 
+	checkWorkflows := true
 	for scanner.Scan() {
 		line := scanner.Text()
-		tokens := strings.Split(line, " ")
-		direction := tokens[0]
-		steps, _ := strconv.Atoi(tokens[1])
-		instruction := Instruction{direction: direction, steps: int64(steps)}
-		digPlan.instructions = append(digPlan.instructions, instruction)
-		hexSteps := tokens[2][2 : len(tokens[2])-2]
-		realSteps, err := strconv.ParseInt(hexSteps, 16, 32)
-		dirCode := tokens[2][len(tokens[2])-2 : len(tokens[2])-1]
-		realDir := ""
-		switch dirCode {
-		case "0":
-			realDir = "R"
-			break
-		case "1":
-			realDir = "D"
-			break
-		case "2":
-			realDir = "L"
-			break
-		case "3":
-			realDir = "U"
-			break
+		if checkWorkflows && line == "" {
+			checkWorkflows = false
 		}
-		realInstruction := Instruction{direction: realDir, steps: realSteps}
+		if checkWorkflows {
+			workflowKey := line[0 : strings.Index(line, "{")-1]
 
-		if err != nil {
-			panic(err)
+			workflow := parseWorkFlow(line[strings.Index(line, "{")+1 : strings.Index(line, "}")-1])
+
+			workFlows.Set(workflowKey, workflow)
+
+		} else {
+
 		}
-		digPlan.realInstructions = append(digPlan.realInstructions, realInstruction)
 	}
 
-	fmt.Println(digPlan)
-
-	return digPlan
+	return &parts, workFlows
 }
