59d08fcd797736e10c881ee29e97ad250bbf015c
Eddie Wood <ewood@blltrust.com>
2023-12-22 16:33:46 -0700

    Day19 done

--- Day19/hello.go
+++ Day19/hello.go
@@ -6,37 +6,29 @@ import (
 	"os"
 	"strconv"
 	"strings"
-
-	orderedmap "github.com/wk8/go-ordered-map/v2"
 )
 
 type PartRange struct {
-	xMin int
-	xMax int
-	mMin int
-	mMax int
-	aMin int
-	aMax int
-	sMin int
-	sMax int
+	max int64
+	min int64
 }
 
 type Part struct {
-	x int
-	m int
-	a int
-	s int
+	x int64
+	m int64
+	a int64
+	s int64
 }
 
-func greaterThan(a, b int) bool {
+func greaterThan(a, b int64) bool {
 	return a > b
 }
 
-func lessThan(a, b int) bool {
+func lessThan(a, b int64) bool {
 	return a < b
 }
 
-func nop(a, b int) bool {
+func nop(a, b int64) bool {
 	return true
 }
 
@@ -47,14 +39,14 @@ const (
 )
 
 type Node struct {
-	function    func(a, b int) bool
+	function    func(a, b int64) bool
 	operator    string
-	operandType string
-	operand2    int
+	operandName string
+	operand     int64
 	next        string
 }
 
-func getFunction(nodeType string) func(a, b int) bool {
+func getFunction(nodeType string) func(a, b int64) bool {
 	switch nodeType {
 	case GreaterThan:
 		return greaterThan
@@ -67,106 +59,91 @@ func getFunction(nodeType string) func(a, b int) bool {
 	}
 }
 
-func (workflow *Workflow) getRanges(partRange *PartRange, workflows *orderedmap.OrderedMap[string, *Workflow]) *[]PartRange {
-
-	partRanges := []PartRange{}
-	operandType := ""
-	for _, node := range *workflow.nodes {
-		operand1Min := -1
-		operand1Max := -1
-		switch node.operandType {
-		case "a":
-			operandType = "a"
-			operand1Min = partRange.aMin
-			operand1Max = partRange.aMax
-			break
-		case "x":
-			operandType = "x"
-			operand1Min = partRange.xMin
-			operand1Max = partRange.xMax
-			break
-		case "m":
-			operandType = "m"
-			operand1Min = partRange.mMin
-			operand1Max = partRange.mMax
-			break
-		case "s":
-			operandType = "s"
-			operand1Min = partRange.sMin
-			operand1Max = partRange.sMax
-		}
-
-		minFits := node.function(operand1Min, node.operand2)
-		maxFits := node.function(operand1Max, node.operand2)
-
-		kindOfFits := true
-		switch node.operator {
-		case GreaterThan:
-			if !maxFits {
-				kindOfFits = false
-				break
-			}
-			if !minFits {
-				operand1Min = node.operand2 + 1
-			}
-			break
-		case LessThan:
-			if !maxFits {
-				operand1Max = node.operand2 - 1
-			}
-			if !minFits {
-				kindOfFits = false
-			}
-		}
-
-		if !kindOfFits {
-			continue
-		}
-
-		partRangeToSend := &PartRange{}
-		partRangeToSend.aMax = partRange.aMax
-		partRangeToSend.aMin = partRange.aMin
-		partRangeToSend.xMax = partRange.xMax
-		partRangeToSend.xMin = partRange.xMin
-		partRangeToSend.mMax = partRange.mMax
-		partRangeToSend.mMin = partRange.mMin
-		partRangeToSend.sMax = partRange.sMax
-		partRangeToSend.sMin = partRange.sMin
-
-		if operandType != "" {
-			switch operandType {
-			case "a":
-				partRangeToSend.aMax = operand1Max
-				partRangeToSend.aMin = operand1Min
-				break
-			case "x":
-				partRangeToSend.xMax = operand1Max
-				partRangeToSend.xMin = operand1Min
-				break
-			case "m":
-				partRangeToSend.mMax = operand1Max
-				partRangeToSend.mMin = operand1Min
-				break
-			case "s":
-				partRangeToSend.sMax = operand1Max
-				partRangeToSend.sMin = operand1Min
-			}
-		}
-
-		if nextNode, present := workflows.Get(node.next); present {
-			partRanges = append(partRanges, *nextNode.getRanges(partRangeToSend, workflows)...)
-		} else if node.next == "A" {
-			partRanges = append(partRanges, *partRangeToSend)
-		}
+func copy(a, b *map[string]int) {
+	for k, v := range *b {
+		(*a)[k] = v
 	}
-	return &partRanges
 }
 
-func (workflow *Workflow) evaluate(part *Part, workflows *orderedmap.OrderedMap[string, *Workflow]) string {
+func maxInt(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func minInt(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func getCombinations(partRanges *map[string]PartRange, next string, workflows *map[string]*Workflow) int64 {
+
+	if next == "A" {
+		var combos int64 = 1
+		for _, partRange := range *partRanges {
+			combos *= partRange.max - partRange.min + 1
+		}
+		return combos
+	} else if next == "R" {
+		return 0
+	}
+
+	workflow, present := (*workflows)[next]
+
+	if !present {
+		panic("workflow not found: " + next)
+	}
+
+	var combos int64 = 0
+	for i := 0; i < len(*workflow.nodes)-1; i++ {
+		node := (*workflow.nodes)[i]
+		min := (*partRanges)[node.operandName].min
+		max := (*partRanges)[node.operandName].max
+
+		var truePartRange, falsePartRange PartRange
+
+		if node.operator == LessThan {
+			truePartRange.min = min
+			truePartRange.max = minInt(node.operand-1, max)
+			falsePartRange.min = maxInt(node.operand, min)
+			falsePartRange.max = max
+		} else if node.operator == GreaterThan {
+			truePartRange.min = maxInt(node.operand+1, min)
+			truePartRange.max = max
+			falsePartRange.min = min
+			falsePartRange.max = minInt(node.operand, max)
+		} else {
+			panic("unknown operator: " + node.operator)
+		}
+
+		if truePartRange.min <= truePartRange.max {
+			copy := make(map[string]PartRange)
+			for k, v := range *partRanges {
+				copy[k] = v
+			}
+			copy[node.operandName] = truePartRange
+			combos += getCombinations(&copy, node.next, workflows)
+		}
+
+		if falsePartRange.min <= falsePartRange.max {
+			(*partRanges)[node.operandName] = falsePartRange
+		} else {
+			break
+		}
+	}
+
+	combos += getCombinations(partRanges, (*(*workflow).nodes)[len((*(*workflow).nodes))-1].next, workflows)
+	return combos
+}
+
+func (workflow *Workflow) evaluate(part *Part, workflows *map[string]*Workflow) string {
 	for _, node := range *workflow.nodes {
 
-		operand1 := -1
-		switch node.operandType {
+		var operand1 int64 = -1
+		switch node.operandName {
 		case "a":
 			operand1 = part.a
 			break
@@ -180,8 +157,8 @@ func (workflow *Workflow) evaluate(part *Part, workflows *orderedmap.OrderedMap[
 			operand1 = part.s
 		}
 
-		if node.function(operand1, node.operand2) {
-			if nextNode, present := workflows.Get(node.next); present {
+		if node.function(operand1, node.operand) {
+			if nextNode, present := (*workflows)[node.next]; present {
 				return nextNode.evaluate(part, workflows)
 			} else {
 				return node.next
@@ -195,9 +172,9 @@ type Workflow struct {
 	nodes *[]Node
 }
 
-func part1(parts *[]Part, workflows *orderedmap.OrderedMap[string, *Workflow]) int {
-	answer := 0
-	inWorkFlow, _ := workflows.Get("in")
+func part1(parts *[]Part, workflows *map[string]*Workflow) int64 {
+	var answer int64 = 0
+	inWorkFlow, _ := (*workflows)["in"]
 
 	for _, part := range *parts {
 		if result := inWorkFlow.evaluate(&part, workflows); result == "A" {
@@ -207,24 +184,16 @@ func part1(parts *[]Part, workflows *orderedmap.OrderedMap[string, *Workflow]) i
 	return answer
 }
 
-func part2(workflows *orderedmap.OrderedMap[string, *Workflow]) int {
-	answer := 0
-	inWorkFlow, _ := workflows.Get("in")
+func part2(workflows *map[string]*Workflow) int64 {
 
-	partRange := PartRange{xMin: 1, xMax: 4000, mMin: 1, mMax: 4000, aMin: 1, aMax: 4000, sMin: 1, sMax: 4000}
-
-	partRanges := inWorkFlow.getRanges(&partRange, workflows)
-
-	for _, partRange := range *partRanges {
-		fmt.Println(partRange)
-		answer += (partRange.xMax - partRange.xMin) * (partRange.mMax - partRange.mMin) * (partRange.aMax - partRange.aMin) * (partRange.sMax - partRange.sMin)
-	}
+	partRanges := map[string]PartRange{"a": {min: 1, max: 4000}, "x": {min: 1, max: 4000}, "m": {min: 1, max: 4000}, "s": {min: 1, max: 4000}}
+	answer := getCombinations(&partRanges, "in", workflows)
 
 	return answer
 }
 
 func main() {
-	parts, workflows := parseFile("testinput.txt")
+	parts, workflows := parseFile("input.txt")
 
 	fmt.Println("answer part 1:", part1(parts, workflows))
 
@@ -237,16 +206,16 @@ func parseWorkFlow(workflowString string) *Workflow {
 	for _, nodestring := range nodestrings {
 		if indexOfColon := strings.Index(nodestring, ":"); indexOfColon > 0 {
 			node := Node{}
-			node.operandType = nodestring[0:1]
+			node.operandName = nodestring[0:1]
 			node.operator = nodestring[1:2]
-			node.operand2, _ = strconv.Atoi(nodestring[2:indexOfColon])
+			node.operand, _ = strconv.ParseInt(nodestring[2:indexOfColon], 10, 64)
 			node.function = getFunction(nodestring[1:2])
 			node.next = nodestring[indexOfColon+1:]
 			*workflow.nodes = append(*workflow.nodes, node)
 		} else {
 			node := Node{}
-			node.operandType = Result
-			node.operand2 = 0
+			node.operandName = Result
+			node.operand = 0
 			node.function = nop
 			node.next = nodestring
 			*workflow.nodes = append(*workflow.nodes, node)
@@ -255,7 +224,7 @@ func parseWorkFlow(workflowString string) *Workflow {
 	return &workflow
 }
 
-func parseFile(fileName string) (*[]Part, *orderedmap.OrderedMap[string, *Workflow]) {
+func parseFile(fileName string) (*[]Part, *map[string]*Workflow) {
 	file, err := os.Open(fileName)
 
 	if err != nil {
@@ -266,7 +235,7 @@ func parseFile(fileName string) (*[]Part, *orderedmap.OrderedMap[string, *Workfl
 
 	scanner := bufio.NewScanner(file)
 
-	workFlows := orderedmap.New[string, *Workflow]()
+	workFlows := make(map[string]*Workflow)
 	parts := []Part{}
 
 	checkWorkflows := true
@@ -281,7 +250,7 @@ func parseFile(fileName string) (*[]Part, *orderedmap.OrderedMap[string, *Workfl
 
 			workflow := parseWorkFlow(line[strings.Index(line, "{")+1 : strings.Index(line, "}")])
 
-			workFlows.Set(workflowKey, workflow)
+			workFlows[workflowKey] = workflow
 
 		} else {
 			part := Part{}
@@ -289,16 +258,16 @@ func parseFile(fileName string) (*[]Part, *orderedmap.OrderedMap[string, *Workfl
 			for _, partString := range partStrings {
 				switch partString[0] {
 				case 'x':
-					part.x, _ = strconv.Atoi(partString[2:])
+					part.x, _ = strconv.ParseInt(partString[2:], 10, 64)
 					break
 				case 'm':
-					part.m, _ = strconv.Atoi(partString[2:])
+					part.m, _ = strconv.ParseInt(partString[2:], 10, 64)
 					break
 				case 'a':
-					part.a, _ = strconv.Atoi(partString[2:])
+					part.a, _ = strconv.ParseInt(partString[2:], 10, 64)
 					break
 				case 's':
-					part.s, _ = strconv.Atoi(partString[2:])
+					part.s, _ = strconv.ParseInt(partString[2:], 10, 64)
 					break
 				default:
 					panic("unknown part type: " + partString[0:1])
@@ -308,5 +277,5 @@ func parseFile(fileName string) (*[]Part, *orderedmap.OrderedMap[string, *Workfl
 		}
 	}
 
-	return &parts, workFlows
+	return &parts, &workFlows
 }
