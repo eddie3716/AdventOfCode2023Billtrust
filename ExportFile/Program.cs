9ce85a1600419ca5c1791cd65f5ac1977142ee34
Eddie Wood <ewood@blltrust.com>
2024-01-12 17:13:34 -0700

    wrote a utility for Quin

---
+++ ExportFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string server = args[0];
+        string database = args[1];
+        string fileId = args[2];
+
+        string connectionString = $"Server={server};Database={database};Trusted_Connection=True;";
+
+        string query = $"SELECT Content FROM [AR].[File] WHERE FileId = @FileId";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@FileId", fileId);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                byte[] data = (byte[])reader[0]; // Assuming the byte array is in the first column
+                File.WriteAllBytes("output.txt", data);
+            }
+        }
+    }
+}
